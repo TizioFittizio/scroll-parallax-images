e36bee65428761d3cfd4b374df991c33fccb8c41
Luca Campana <luca.campa.94@gmail.com>
2019-11-09 17:28:20 +0100

    Implemented scale effetc on images

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-const NUMBER_OF_IMAGES = 1;
+const NUMBER_OF_IMAGES = 10;
 
 class ImageScaledOnScroll {
 
     static IMAGE_SCALE_START_VALUE = 1;
-    static IMAGE_SCALE_END_VALUE = 0.5;
-    static IMAGE_SCALE_RANGE_PIXELS = 250;
+    static IMAGE_SCALE_END_VALUE = 0.6;
+    static IMAGE_SCALE_RANGE_PIXELS = 800;
 
     constructor(values){
         this._htmlElement = values.htmlElement;
@@ -13,11 +13,30 @@ class ImageScaledOnScroll {
 
     update(){
         const centerYAbsolute = this._computeCenterYAbsolute();
+        const pageCenterYAbsolute = window.scrollY + (window.innerHeight / 2);
+        const distance = Math.abs(centerYAbsolute - pageCenterYAbsolute);
+        const scaleValue = this._computeScaleValueFromDistance(distance);
+        this._setScaleStyleToHTMLElement(scaleValue);
     }
 
     _computeCenterYAbsolute(){
         const rect = this._htmlElement.getBoundingClientRect();
-        console.log(rect);
+        const topYAbsolute = rect.y + window.scrollY;
+        const centerYAbsolute = topYAbsolute + rect.height / 2;
+        return centerYAbsolute;
+    }
+
+    _computeScaleValueFromDistance(distance){
+        const ratio = distance / ImageScaledOnScroll.IMAGE_SCALE_RANGE_PIXELS;
+        const totalRange = ImageScaledOnScroll.IMAGE_SCALE_START_VALUE - ImageScaledOnScroll.IMAGE_SCALE_END_VALUE;
+        return Math.min(
+            ImageScaledOnScroll.IMAGE_SCALE_START_VALUE, 
+            (totalRange * ratio) + ImageScaledOnScroll.IMAGE_SCALE_END_VALUE
+        );
+    }
+
+    _setScaleStyleToHTMLElement(scaleValue){
+        this._htmlElement.style.transform = `scale(${scaleValue})`
     }
 
 }
@@ -56,7 +75,8 @@ const onScroll = e => {
 
 const updateTriggerPosition = () => {
     const trigger = document.getElementsByClassName('trigger')[0];
-    trigger.setAttribute('value', `Y: ${window.scrollY}`);
+    const windowCenterYRelative = window.scrollY + (window.innerHeight / 2);
+    trigger.setAttribute('value', `ScrollY: ${window.scrollY} --- ScrollY + 1/2: ${windowCenterYRelative}`);
 }
 
 document.addEventListener('DOMContentLoaded', main);
