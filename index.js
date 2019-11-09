4e3d30aeb109ff8c787573c06c1b3488ddda68a2
Luca Campana <luca.campa.94@gmail.com>
2019-11-09 16:27:22 +0100

    Refactoring added ImageScaledOnScroll

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 
-const NUMBER_OF_IMAGES = 2;
-const IMAGE_SCALE_START_VALUE = 1;
-const IMAGE_SCALE_END_VALUE = 0.5;
-const IMAGE_SCALE_RANGE_PIXELS = 250;
+const NUMBER_OF_IMAGES = 1;
 
-const main = () => {
-    const HTMLImageContainers = [...new Array(NUMBER_OF_IMAGES)].map(generateHTMLImageContainer);
-    const HTMLBlockImages = getHTMLBlockImages();
-    HTMLImageContainers.forEach(x => HTMLBlockImages.appendChild(x));
+class ImageScaledOnScroll {
+
+    static IMAGE_SCALE_START_VALUE = 1;
+    static IMAGE_SCALE_END_VALUE = 0.5;
+    static IMAGE_SCALE_RANGE_PIXELS = 250;
+
+    constructor(values){
+        this._htmlElement = values.htmlElement;
+    }
+
+    update(){
+        const centerYAbsolute = this._computeCenterYAbsolute();
+    }
+
+    _computeCenterYAbsolute(){
+        const rect = this._htmlElement.getBoundingClientRect();
+        console.log(rect);
+    }
+
 }
 
-const generateHTMLImageContainer = () => {
-    const container = document.createElement('div');
-    container.className = 'image-container';
-    container.appendChild(generateHTMLImage());
-    return container;
+let imagesScaledOnScroll = [];
+
+const main = () => {
+    const HTMLImages = [...new Array(NUMBER_OF_IMAGES)].map(generateHTMLImage);
+    const HTMLImageContainers = HTMLImages.map(generateHTMLImageContainer);
+    const HTMLBlockImages = getHTMLBlockImages();
+    HTMLImageContainers.forEach(x => HTMLBlockImages.appendChild(x));
+    imagesScaledOnScroll = HTMLImages.map(x => new ImageScaledOnScroll({ htmlElement: x }));
 }
 
 const generateHTMLImage = () => {
@@ -23,8 +38,26 @@ const generateHTMLImage = () => {
     return image;
 }
 
+const generateHTMLImageContainer = HTMLImage => {
+    const container = document.createElement('div');
+    container.className = 'image-container';
+    container.appendChild(HTMLImage);
+    return container;
+}
+
 const getHTMLBlockImages = () => {
     return document.getElementsByClassName('block-images')[0];
 }
 
-main();
+const onScroll = e => {
+    imagesScaledOnScroll.forEach(x => x.update());
+    updateTriggerPosition();
+}
+
+const updateTriggerPosition = () => {
+    const trigger = document.getElementsByClassName('trigger')[0];
+    trigger.setAttribute('value', `Y: ${window.scrollY}`);
+}
+
+document.addEventListener('DOMContentLoaded', main);
+document.addEventListener('scroll', onScroll);
